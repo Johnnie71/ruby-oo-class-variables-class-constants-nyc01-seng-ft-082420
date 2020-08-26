427ffd7148c726b4ac58338737f2ba2bd1b5e640
johnnie71 <johnnie.agonz71@gmail.com>
2020-08-25 22:35:06 -0400

    Created Class Constants, and filter in shoe.rb

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,18 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq! # (uniq!)returns new array only if they're are repeats, if not it will => nil
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
 
 end
